9f3936296a87da8f3c2c05a8accf322664294e91
Achira2001 <achira.20220024@iit.ac.lk>
2024-08-29 12:44:08 +0530

    update files

---
+++ src/constants/index.js
@@ -0,0 +1,61 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+import project5 from "../assets/projects/project-5.jpg";
+import project6 from "../assets/projects/project-6.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React as well as back-end technologies like Node.js, MySQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React.js, Node.js, MySQL and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const PROJECTS = [
+  {
+    title: "Saraya Products And Agent Management System",
+    image: project1,
+    description:
+      "####",
+    technologies: ["HTML", "CSS", "JavaScript", "React.js", "Node.js", "Express.js", "MongoDB"],
+  },
+  {
+    title: "Food Delivery App",
+    image: project2,
+    description:
+      "####",
+    technologies: ["HTML", "CSS", "JavaScript", "React.js", "Node.js", "Express.js", "MongoDB"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Theatre Booking System",
+    image: project4,
+    description:
+      "#####",
+    technologies: ["Java"],
+  },
+  {
+    title: "CEYTOUR Tourist Guid Website about Sri Lanka",
+    image: project5,
+    description:
+      "#####",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "University Academic Progression Predicting Application",
+    image: project6,
+    description:
+      "#####",
+    technologies: ["Java"],
+  },
+];
+
+export const CONTACT = {
+  address: "Colombo, Sri Lanka",
+  phoneNo: "+94 76 493 7117 ",
+  email: "achirashashik1@gmail.com",
+};
